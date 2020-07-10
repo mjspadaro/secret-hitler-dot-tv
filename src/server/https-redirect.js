aa9e761add8fbffafbd35d9c35e4d84aa1afbb84
mjspadaro <michael@mjspadaro.com>
2020-07-10 16:07:49 -0400

    removed redirection console log message

--- src/server/https-redirect.js
+++ src/server/https-redirect.js
@@ -7,7 +7,6 @@ module.exports = (req, res, next) => {
   if (req.protocol != 'https' && process.env.NODE_ENV == 'production') {
     var parts = req.get('host').split(':');
     var host = parts[0] || '127.0.0.1';
-    console.log('Redirecting: ' + 'https://' + host + req.url);
     return res.redirect('https://' + host + req.url);
   }
   next();
